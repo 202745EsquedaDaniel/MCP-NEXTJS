9b039324256e03921b993b84785cb77787a5b728
202745EsquedaDaniel <eduardani84@gmail.com>
2025-07-14 16:36:02 -0700

    [ADD] - Next + Mcp

---
+++ src/app/[transport]/route.ts
@@ -0,0 +1,40 @@
+import { createMcpHandler } from "@vercel/mcp-adapter";
+import { z } from "zod";
+
+const handler = createMcpHandler(server => {
+    server.tool(
+        "courseRecommender",
+        "Give a course recommendation based on experience level",
+        {
+            experienceLevel: z.enum(["beginner", "intermediate","advanced"])
+        },
+        ({ experienceLevel }) => ({
+            content: [
+                {
+                    type: "text",
+                    text: `I recommend you take the ${
+                        experienceLevel === "beginner"
+                            ? "Professional Javascript"
+                            : "Professional React & Next.js"
+                    } course.`
+                }
+            ]
+        })
+    )
+}, {
+    capabilities: {
+        tools:{
+            courseRecommender: {
+                description : "Give a course recommendation based on experience level"
+            }
+        }
+    }
+}, {
+    redisUrl: process.env.REDIS_URL,
+    sseEndpoint: "/sse",
+    streamableHttpEndpoint: "/mcp",
+    verboseLogs: true,
+    maxDuration: 60,
+})
+
+export { handler as GET, handler as POST, handler as DELETE}
